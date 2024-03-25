e7a5a8af3cd4402727e1ba7d621ffff3a1ca5771
liwanli <liyuhan20240319@aliyun.com>
2024-03-25 23:05:02 +0800

    4.4修改启动日志，增加/member，修改logback日志

--- member/src/main/java/com/jiawa/config/MemberApplication.java
+++ member/src/main/java/com/jiawa/config/MemberApplication.java
@@ -1,8 +1,12 @@
 package com.jiawa.config;
 
+//import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
 /**
  * ComponentScan：启动类只能扫描到同包下的类，不能只写com，会把第三方的jar包也扫描得到，可能会有问题。
@@ -10,9 +14,13 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan("com.jiawa")
 public class MemberApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(MemberApplication.class);
 
-    public static void main(String[] args) {
-        SpringApplication.run(MemberApplication.class, args);
-    }
-
+        public static void main(String[] args){
+            SpringApplication app = new SpringApplication(MemberApplication.class);
+            Environment env = app.run(args).getEnvironment();
+            LOG.info("启动成功！！");
+            LOG.info("测试地址: \thttp://127.0.0.1:{}/hello", env.getProperty("server.port"));
+        }
 }
+
