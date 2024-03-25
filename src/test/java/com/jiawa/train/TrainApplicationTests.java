a6019e58d27d6d6a6a71f47296e4d6bf2c196ad7
liwanli <liyuhan20240319@aliyun.com>
2024-03-25 22:43:09 +0800

    初始化

---
+++ src/test/java/com/jiawa/train/TrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiawa.train;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
