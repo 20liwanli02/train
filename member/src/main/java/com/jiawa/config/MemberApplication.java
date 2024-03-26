b480bda2850fa946d7eb04070937d0c4cb66ead4
liwanli <liyuhan20240319@aliyun.com>
2024-03-26 14:07:33 +0800

    4.6增加测试文件，方便直接在idea中测试

--- member/src/main/java/com/jiawa/config/MemberApplication.java
+++ member/src/main/java/com/jiawa/config/MemberApplication.java
@@ -20,7 +20,7 @@ public class MemberApplication {
             SpringApplication app = new SpringApplication(MemberApplication.class);
             Environment env = app.run(args).getEnvironment();
             LOG.info("启动成功！！");
-            LOG.info("测试地址: \thttp://127.0.0.1:{}/hello", env.getProperty("server.port"));
+            LOG.info("测试地址: \thttp://127.0.0.1:{}{}/hello", env.getProperty("server.port"), env.getProperty("server.servlet.context-path"));
         }
 }
 
