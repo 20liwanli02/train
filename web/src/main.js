94bdb558d00f377f2a5830cba61b886a5c32b89a
liwanli <liyuhan20240319@aliyun.com>
2024-03-29 13:00:23 +0800

    5.4集成Ant Design Vue及其图标

--- web/src/main.js
+++ web/src/main.js
@@ -3,4 +3,21 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+//导入ant-design-vue组件架子
+import Antd from 'ant-design-vue';
+//导入ant-design-vue组件样式
+import 'ant-design-vue/dist/antd.css';
+//导入ant-design-vue组件图标
+import * as Icons from '@ant-design/icons-vue';
+
+//导入，引用，使用
+
+//引入ant-design-vue组件--.use(Antd)
+const app = createApp(App);
+app.use(Antd).use(store).use(router).mount('#app')
+
+//全局引用ant-design-vue组件图标
+const icons = Icons;
+for(const i in icons){
+    app.component(i,icons[i])
+}
--- web/src/views/AboutView.vue
+++ web/src/views/AboutView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <a-button>登录</a-button><edit-outlined />
   </div>
 </template>
