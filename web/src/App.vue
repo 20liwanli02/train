07ecb3340fb70cb9db53347359d77567b94209d6
liwanli <liyuhan20240319@aliyun.com>
2024-03-29 20:49:01 +0800

    5.6增加login路由及页面

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,11 +1,12 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/login">login</router-link>
   </nav>
   <router-view/>
 </template>
 
++
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
