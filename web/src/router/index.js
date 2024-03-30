be65a6b14e4f4f98ab881e95c782d1e2bc2cf0e4
liwanli <liyuhan20240319@aliyun.com>
2024-03-30 14:34:34 +0800

    5.12增加控台主页

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,20 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/login',
-    name: 'login',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/login.vue')
-  }
+    {
+        path: '/login',
+        name: 'login',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../views/login.vue')
+    },
+    {
+        path: '/',
+        name: 'main',
+        component: () => import('../views/main.vue')
+    },
 ]
 
 const router = createRouter({
--- web/src/views/AboutView.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-    <a-button>登录</a-button><edit-outlined />
-  </div>
-</template>
