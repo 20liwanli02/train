0823ded7550eea3407b15f68be2d9dad086d8f5d
liwanli <liyuhan20240319@aliyun.com>
2024-04-02 21:03:46 +0800

    7.6修改菜单，实现页面切换

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -7,16 +7,25 @@
         退出登录
       </router-link>
     </div>
+
     <a-menu
         v-model:selectedKeys="selectedKeys1"
         theme="dark"
         mode="horizontal"
         :style="{ lineHeight: '64px' }"
     >
-      <a-menu-item key="1">nav 1</a-menu-item>
-      <a-menu-item key="2">nav 2</a-menu-item>
-      <a-menu-item key="3">nav 3</a-menu-item>
+      <a-menu-item key="/welcome">
+        <router-link to="/welcome">
+          <coffee-outlined/> &nbsp; 欢迎
+        </router-link>
+      </a-menu-item>
+      <a-menu-item key="/passenger">
+        <router-link to="/passenger">
+          <coffee-outlined/> &nbsp; 乘车人管理
+        </router-link>
+      </a-menu-item>
     </a-menu>
+
   </a-layout-header>
 
 </template>
--- web/src/components/the-sider.vue
+++ web/src/components/the-sider.vue
@@ -6,57 +6,25 @@
         mode="inline"
         :style="{ height: '100%', borderRight: 0 }"
     >
-      <a-sub-menu key="sub1">
-        <template #title>
-              <span>
-                <user-outlined />
-                subnav 1
-              </span>
-        </template>
-        <a-menu-item key="1">option1</a-menu-item>
-        <a-menu-item key="2">option2</a-menu-item>
-        <a-menu-item key="3">option3</a-menu-item>
-        <a-menu-item key="4">option4</a-menu-item>
-      </a-sub-menu>
-      <a-sub-menu key="sub2">
-        <template #title>
-              <span>
-                <laptop-outlined />
-                subnav 2
-              </span>
-        </template>
-        <a-menu-item key="5">option5</a-menu-item>
-        <a-menu-item key="6">option6</a-menu-item>
-        <a-menu-item key="7">option7</a-menu-item>
-        <a-menu-item key="8">option8</a-menu-item>
-      </a-sub-menu>
-      <a-sub-menu key="sub3">
-        <template #title>
-              <span>
-                <notification-outlined />
-                subnav 3
-              </span>
-        </template>
-        <a-menu-item key="9">option9</a-menu-item>
-        <a-menu-item key="10">option10</a-menu-item>
-        <a-menu-item key="11">option11</a-menu-item>
-        <a-menu-item key="12">option12</a-menu-item>
-      </a-sub-menu>
+      <a-menu-item key="/welcome">
+        <router-link to="/welcome">
+          <coffee-outlined/> &nbsp; 欢迎
+        </router-link>
+      </a-menu-item>
+      <a-menu-item key="/passenger">
+        <router-link to="/passenger">
+          <coffee-outlined/> &nbsp; 乘车人管理
+        </router-link>
+      </a-menu-item>
     </a-menu>
   </a-layout-sider>
 </template>
 
 <script>
 import {defineComponent, ref} from 'vue';
-// import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons-vue';
 
 export default defineComponent({
   name: "the-sider-view",
-  // components: {
-  //   UserOutlined,
-  //   LaptopOutlined,
-  //   NotificationOutlined,
-  // },
   setup() {
     return {
       selectedKeys2: ref(['1']),
