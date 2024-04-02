50d84eb792e1793205179c4c58da318255c73527
liwanli <liyuhan20240319@aliyun.com>
2024-04-02 22:03:41 +0800

    7.7增加乘车人表单

--- web/src/views/main/passenger.vue
+++ web/src/views/main/passenger.vue
@@ -1,19 +1,69 @@
 <template>
   <a-button type="primary" @click="showModal">新增</a-button>
-  <a-modal v-model:visible="visible" title="乘车人" @ok="handleOk">
-    <p>Some contents...</p>
-    <p>Some contents...</p>
-    <p>Some contents...</p>
+  <a-modal v-model:visible="visible" title="乘车人" @ok="handleOk" ok-text="确认" cancel-text="取消">
+    <a-form :model="passenger" :label-col="{span: 4}" :wrapper-col="{ span: 20 }">
+      <a-form-item label="姓名">
+        <a-input v-model:value="passenger.name" />
+      </a-form-item>
+      <a-form-item label="身份证">
+        <a-input v-model:value="passenger.idCard" />
+      </a-form-item>
+      <a-form-item label="旅客类型">
+        <a-select v-model:value="passenger.type">
+          <a-select-option value="1">成人</a-select-option>
+          <a-select-option value="2">儿童</a-select-option>
+          <a-select-option value="3">学生</a-select-option>
+        </a-select>
+      </a-form-item>
+    </a-form>
   </a-modal>
 </template>
 
 
 <script>
-import {defineComponent,ref} from "vue";
+import {defineComponent,ref,reactive} from "vue";
 
 export default defineComponent({
   setup() {
     const visible = ref(false);
+    const passenger = reactive({
+      id: undefined,
+      memberId: undefined,
+      name: undefined,
+      idCard: undefined,
+      type: undefined,
+      createTime: undefined,
+      updateTime: undefined,
+    });
+    // const passengers = ref([]);
+    // // 分页的三个属性名是固定的
+    // const pagination = ref({
+    //   total: 0,
+    //   current: 1,
+    //   pageSize: 10,
+    // });
+    // let loading = ref(false);
+    // const columns = [
+    //   {
+    //     title: '姓名',
+    //     dataIndex: 'name',
+    //     key: 'name',
+    //   },
+    //   {
+    //     title: '身份证',
+    //     dataIndex: 'idCard',
+    //     key: 'idCard',
+    //   },
+    //   {
+    //     title: '旅客类型',
+    //     dataIndex: 'type',
+    //     key: 'type',
+    //   },
+    //   {
+    //     title: '操作',
+    //     dataIndex: 'operation'
+    //   }
+    // ];
 
     const showModal = () => {
       visible.value = true;
@@ -27,6 +77,7 @@ export default defineComponent({
       visible,
       showModal,
       handleOk,
+      passenger,
     };
   },
 })
