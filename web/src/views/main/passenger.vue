f09d192e5e87c9a78dd11d91a92d7501dbc238b9
liwanli <liyuhan20240319@aliyun.com>
2024-04-02 21:47:38 +0800

    7.7乘车人新增界面开发，增加新增按钮

--- web/src/views/main/passenger.vue
+++ web/src/views/main/passenger.vue
@@ -1,15 +1,32 @@
 <template>
-  <h1>乘车人管理</h1>
+  <a-button type="primary" @click="showModal">新增</a-button>
+  <a-modal v-model:visible="visible" title="乘车人" @ok="handleOk">
+    <p>Some contents...</p>
+    <p>Some contents...</p>
+    <p>Some contents...</p>
+  </a-modal>
 </template>
 
 
 <script>
-import {defineComponent} from "vue";
+import {defineComponent,ref} from "vue";
 
 export default defineComponent({
   setup() {
-    return {
+    const visible = ref(false);
 
+    const showModal = () => {
+      visible.value = true;
+    };
+    const handleOk = e => {
+      console.log(e);
+      visible.value = false;
+    };
+
+    return {
+      visible,
+      showModal,
+      handleOk,
     };
   },
 })
