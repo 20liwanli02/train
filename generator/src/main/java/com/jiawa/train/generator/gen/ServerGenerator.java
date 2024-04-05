b74152dcafdd40e747205ff3f3265b8ef0e7ac47
liwanli <liyuhan20240319@aliyun.com>
2024-04-05 23:33:45 +0800

    8.7增加DbUtil，获取表字段信息

--- generator/src/main/java/com/jiawa/train/generator/gen/ServerGenerator.java
+++ generator/src/main/java/com/jiawa/train/generator/gen/ServerGenerator.java
@@ -1,5 +1,7 @@
 package com.jiawa.train.generator.gen;
 
+import com.jiawa.train.generator.util.DbUtil;
+import com.jiawa.train.generator.util.Field;
 import com.jiawa.train.generator.util.FreemarkerUtil;
 import freemarker.template.TemplateException;
 import org.dom4j.Document;
@@ -9,6 +11,7 @@ import org.dom4j.io.SAXReader;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ServerGenerator {
@@ -34,6 +37,17 @@ public class ServerGenerator {
         Node domainObjectName = table.selectSingleNode("@domainObjectName");
         System.out.println(tableName.getText() + "/" + domainObjectName.getText());
 
+        // 为DbUtil设置数据源
+        Node connectionURL = document.selectSingleNode("//@connectionURL");
+        Node userId = document.selectSingleNode("//@userId");
+        Node password = document.selectSingleNode("//@password");
+        System.out.println("url: " + connectionURL.getText());
+        System.out.println("user: " + userId.getText());
+        System.out.println("password: " + password.getText());
+        DbUtil.url = connectionURL.getText();
+        DbUtil.user = userId.getText();
+        DbUtil.password = password.getText();
+
         // 示例：表名 jiawa_test
         // Domain = JiawaTest，Passenger
         String Domain = domainObjectName.getText();
@@ -41,6 +55,10 @@ public class ServerGenerator {
         String domain = Domain.substring(0, 1).toLowerCase() + Domain.substring(1);
         // do_main = jiawa-test
         String do_main = tableName.getText().replaceAll("_", "-");
+        // 表中文名
+        String tableNameCn = DbUtil.getTableComment(tableName.getText());
+        List<Field> fieldList = DbUtil.getColumnByTableName(tableName.getText());
+//        Set<String> typeSet = getJavaTypes(fieldList);
 
         // 组装参数
         Map<String, Object> param = new HashMap<>();
