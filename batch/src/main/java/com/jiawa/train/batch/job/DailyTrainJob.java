8d631237bf2f228b78d47d93c8beab1c0cb0becb
liwanli <liyuhan20240319@aliyun.com>
2024-04-13 18:54:51 +0800

    11.7，增加日志流水号，跑批：前端的请求只做一个维护作用，对任务维护的请求不会经过日志拦截器。

--- batch/src/main/java/com/jiawa/train/batch/job/DailyTrainJob.java
+++ batch/src/main/java/com/jiawa/train/batch/job/DailyTrainJob.java
@@ -1,12 +1,14 @@
 package com.jiawa.train.batch.job;
 
 
+import cn.hutool.core.util.RandomUtil;
 import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 
 @DisallowConcurrentExecution
 public class DailyTrainJob implements Job {
@@ -19,7 +21,7 @@ public class DailyTrainJob implements Job {
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
         // 增加日志流水号
-//        MDC.put("LOG_ID", System.currentTimeMillis() + RandomUtil.randomString(3));
+        MDC.put("LOG_ID", System.currentTimeMillis() + RandomUtil.randomString(3));
         LOG.info("生成15天后的车次数据开始");
 //        Date date = new Date();
 //        DateTime dateTime = DateUtil.offsetDay(date, 15);
