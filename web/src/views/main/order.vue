194ea068834d38728448422bebfc6471f9111ce3
liwanli <liyuhan20240319@aliyun.com>
2024-04-18 22:15:55 +0800

    12.7美化车次信息的显示

--- web/src/views/main/order.vue
+++ web/src/views/main/order.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>{{ dailyTrainTicket }}</div>
+  <div class="order-train">
+    <span class="order-train-main">{{dailyTrainTicket.date}}</span>&nbsp;
+    <span class="order-train-main">{{dailyTrainTicket.trainCode}}</span>次&nbsp;
+    <span class="order-train-main">{{dailyTrainTicket.start}}</span>站
+    <span class="order-train-main">({{dailyTrainTicket.startTime}})</span>&nbsp;
+    <span class="order-train-main">——</span>&nbsp;
+    <span class="order-train-main">{{dailyTrainTicket.end}}</span>站
+    <span class="order-train-main">({{dailyTrainTicket.endTime}})</span>&nbsp;
+  </div>
 </template>
 
 <script>
@@ -17,3 +25,41 @@ export default defineComponent({
   },
 });
 </script>
+
+<style>
+.order-train .order-train-main {
+  font-size: 18px;
+  font-weight: bold;
+}
+.order-train .order-train-ticket {
+  margin-top: 15px;
+}
+.order-train .order-train-ticket .order-train-ticket-main {
+  color: red;
+  font-size: 18px;
+}
+
+.order-tickets {
+  margin: 10px 0;
+}
+.order-tickets .ant-col {
+  padding: 5px 10px;
+}
+.order-tickets .order-tickets-header {
+  background-color: cornflowerblue;
+  border: solid 1px cornflowerblue;
+  color: white;
+  font-size: 16px;
+  padding: 5px 0;
+}
+.order-tickets .order-tickets-row {
+  border: solid 1px cornflowerblue;
+  border-top: none;
+  vertical-align: middle;
+  line-height: 30px;
+}
+
+.order-tickets .choose-seat-item {
+  margin: 5px 5px;
+}
+</style>
