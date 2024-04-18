dc353e6d881229efcafced082c67eadf4e740935
liwanli <liyuhan20240319@aliyun.com>
2024-04-18 21:14:09 +0800

    12.6会员端增加余票查询页面，功能与控台页面一致

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -25,7 +25,11 @@ const routes = [
         },{
             path: 'passenger',
             component: () => import('../views/main/passenger.vue')
-        },]
+        },{
+            path: 'ticket',
+            component: () => import('../views/main/ticket.vue')
+        },
+        ]
     },
     {
         path: '',
