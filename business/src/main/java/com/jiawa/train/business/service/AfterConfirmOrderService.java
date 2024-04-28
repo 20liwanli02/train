cc33e605226caa7b023680f0d4aa51362e9c774e
liwanli <liyuhan20240319@aliyun.com>
2024-04-28 17:08:24 +0800

    12.22处理售卖后的余票数量

--- business/src/main/java/com/jiawa/train/business/service/AfterConfirmOrderService.java
+++ business/src/main/java/com/jiawa/train/business/service/AfterConfirmOrderService.java
@@ -1,7 +1,9 @@
 package com.jiawa.train.business.service;
 
 import com.jiawa.train.business.domain.DailyTrainSeat;
+import com.jiawa.train.business.domain.DailyTrainTicket;
 import com.jiawa.train.business.mapper.DailyTrainSeatMapper;
+import com.jiawa.train.business.mapper.cust.DailyTrainTicketMapperCust;
 import jakarta.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,15 +19,49 @@ public class AfterConfirmOrderService {
 
      @Resource
      private DailyTrainSeatMapper dailyTrainSeatMapper;
+     @Resource
+     private DailyTrainTicketMapperCust dailyTrainTicketMapperCust;
 
      @Transactional
-    public void AfterDoConfirm(List<DailyTrainSeat> finalSeatList){
+    public void AfterDoConfirm(DailyTrainTicket dailyTrainTicket, List<DailyTrainSeat> finalSeatList){
          for (DailyTrainSeat dailyTrainSeat : finalSeatList) {
              DailyTrainSeat seatForUpdate = new DailyTrainSeat();
              seatForUpdate.setId(dailyTrainSeat.getId());
              seatForUpdate.setSell(dailyTrainSeat.getSell());
              seatForUpdate.setUpdateTime(new Date());
              dailyTrainSeatMapper.updateByPrimaryKeySelective(seatForUpdate);
+
+             Integer startIndex = dailyTrainTicket.getStartIndex();
+             Integer endIndex = dailyTrainTicket.getEndIndex();
+             char[] chars = seatForUpdate.getSell().toCharArray();
+             Integer maxStartIndex = endIndex - 1;
+             Integer minEndIndex = startIndex + 1;
+             Integer minStartIndex = 0;
+             for (int i = startIndex - 1; i >= 0 ; i--) {
+                 char aChar = chars[i];
+                 if(aChar == '1'){
+                     minStartIndex = i + 1;
+                     break;
+                 }
+             }
+             LOG.info("影响出发站区间：" + minStartIndex + "-" + maxStartIndex);
+             Integer maxEndIndex = seatForUpdate.getSell().length();
+             for (int i = endIndex; i < seatForUpdate.getSell().length(); i++) {
+                 char aChar = chars[i];
+                 if (aChar == '1') {
+                     maxEndIndex = i;
+                     break;
+                 }
+             }
+             LOG.info("影响到达站区间：" + minEndIndex + "-" + maxEndIndex);
+             dailyTrainTicketMapperCust.updateCountBySell(
+                     dailyTrainSeat.getDate(),
+                     dailyTrainSeat.getTrainCode(),
+                     dailyTrainSeat.getSeatType(),
+                     minStartIndex,
+                     maxStartIndex,
+                     minEndIndex,
+                     maxEndIndex);
          }
     }
 }
