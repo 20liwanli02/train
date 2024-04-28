398fce5a4795ea120764c58f12ff592cccf0b394
liwanli <liyuhan20240319@aliyun.com>
2024-04-28 21:20:00 +0800

    13.4读取配置文件的配置（nacos）

--- member/src/main/java/com/jiawa/train/member/controller/TestController.java
+++ member/src/main/java/com/jiawa/train/member/controller/TestController.java
@@ -1,12 +1,17 @@
 package com.jiawa.train.member.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    @Value("${test.nacos}")
+    private String testNacos;
+
     @GetMapping("/hello")
     public String hello(){
-    return "Hello World！";
+        return String.format("HELLO %s!", testNacos);
     }
 }
