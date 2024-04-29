fb11bdc19442fa3a569c3ea305b74582e8dec438
liwanli <liyuhan20240319@aliyun.com>
2024-04-29 22:49:29 +0800

    13.4集成nacos配置中心功能。实现动态刷新

--- member/src/main/java/com/jiawa/train/member/config/MemberApplication.java
+++ member/src/main/java/com/jiawa/train/member/config/MemberApplication.java
@@ -1,6 +1,5 @@
 package com.jiawa.train.member.config;
 
-//import org.mybatis.spring.annotation.MapperScan;
 import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,21 +8,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
-/**
- * ComponentScan：扫描Controller，启动类只能扫描到同包下的类，不能只写com，会把第三方的jar包也扫描得到，可能会有问题。
- * MapperScan：扫描Mapper
- */
 @SpringBootApplication
 @ComponentScan("com.jiawa")
-@MapperScan("com.jiawa.train.member.mapper")
+@MapperScan("com.jiawa.train.*.mapper")
 public class MemberApplication {
+
     private static final Logger LOG = LoggerFactory.getLogger(MemberApplication.class);
 
-        public static void main(String[] args){
-            SpringApplication app = new SpringApplication(MemberApplication.class);
-            Environment env = app.run(args).getEnvironment();
-            LOG.info("启动成功！！");
-            LOG.info("测试地址: \thttp://127.0.0.1:{}{}/hello", env.getProperty("server.port"), env.getProperty("server.servlet.context-path"));
-        }
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(MemberApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("测试地址: \thttp://127.0.0.1:{}{}/hello", env.getProperty("server.port"), env.getProperty("server.servlet.context-path"));
+    }
 }
-
